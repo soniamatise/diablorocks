293c05d8708b0836621848dbe531201d2633bb36
Maurits Meester <maurits@matise.nl>
2018-03-03 17:26:45 +0100

    implemented matise grid

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,14 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
+		<div class="row">
+			<div class="small-12 column">
+				hoi 1
+			</div>
+			<div class="small-12 column">
+				hoi 1
+			</div>
+		</div>
   </div>
 </template>
 
@@ -11,13 +19,6 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  @import 'assets/scss/app.scss';
 </style>
