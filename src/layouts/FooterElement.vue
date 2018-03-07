7d5d8f9620362314aa1ad9e31086bbb2d7e42296
Sidstumple <cydstumpel@msn.com>
2018-03-07 17:09:35 +0100

    add  margins & paddings to footer

--- src/layouts/FooterElement.vue
+++ src/layouts/FooterElement.vue
@@ -1,11 +1,10 @@
 <template>
 	<footer class="footer">
 		<section class="row footer__title">
-			<h3>Not your average Digital Agency</h3>
+			<h3>Not your average Digital Agency.</h3>
 		</section>
 
-		<section class="row footer__nav">
-			<ul class="row list--row footer__nav__list">
+			<ul class="row list--row footer__nav">
 				<li class="mobile-hide footer__nav__list-item"><a href="/culture">Culture</a></li>
 				<li class="mobile-hide footer__nav__list-item"><a href="/work">Work</a></li>
 				<li class="mobile-hide footer__nav__list-item"><a href="/who-we-are">Who we are</a></li>
@@ -13,31 +12,28 @@
 				<li class="footer__nav__list-item"><a href="/news">News</a></li>
 				<li class="footer__nav__list-item"><a href="/careers">Careers</a></li>
 			</ul>
-		</section>
 
-		<section class="row footer__contact">
-				<ul class="column list--column footer__contact__list">
-					<li class="footer__contact__list-item">office@matise.nl</li>
-					<li class="footer__contact__list-item">+31 20 845 37 99</li>
-					<li class="footer__contact__list-item">Entrepotdok 78, Amsterdam</li>
-					<li class="footer__contact__list-item">Open till 6pm today</li>
+		<section class="row footer__contact-us">
+				<ul class="column list--column footer__contact">
+					<li class="footer__contact__item">office@matise.nl</li>
+					<li class="footer__contact__item">+31 20 845 37 99</li>
+					<li class="footer__contact__item">Entrepotdok 78, Amsterdam</li>
+					<li class="footer__contact__item">Open till 6pm today</li>
 				</ul>
 
 				<ul class="column list--column footer__social">
-					<li class="footer__social__item"><a href="https://www.instagram.com/matiseamsterdam/">Instagram</a></li>
-					<li class="footer__social__item"><a href="https://www.facebook.com/MatiseAmsterdam/">Facebook</a></li>
-					<li class="footer__social__item"><a href="https://www.linkedin.com/company/matise/">LinkedIn</a></li>
-					<li class="footer__social__item"><a href="https://github.com/MatiseAms">Github</a></li>
+					<li class="footer__social__item link--external"><a href="https://www.instagram.com/matiseamsterdam/">Instagram</a></li>
+					<li class="footer__social__item link--external"><a href="https://www.facebook.com/MatiseAmsterdam/">Facebook</a></li>
+					<li class="footer__social__item link--external"><a href="https://www.linkedin.com/company/matise/">LinkedIn</a></li>
+					<li class="footer__social__item link--external"><a href="https://github.com/MatiseAms">Github</a></li>
 				</ul>
 		</section>
 
-		<section class="row footer__practical">
-			<ul class="list--row footer__practical__list">
-				<li class="footer__practical__list-item">© Matise 2018 — All Rights Reserved</li>
-				<li class="footer__practical__list-item"><a href="#">Terms & conditions</a></li>
-				<li class="footer__practical__list-item"><a href="#">Privacy Policy</a></li>
-			</ul>
-		</section>
+		<ul class="row list--row footer__practical">
+			<li class="footer__practical__item">© Matise 2018 — All Rights Reserved</li>
+			<li class="footer__practical__item"><a href="#">Terms & conditions</a></li>
+			<li class="footer__practical__item"><a href="#">Privacy Policy</a></li>
+		</ul>
 	</footer>
 </template>
 
