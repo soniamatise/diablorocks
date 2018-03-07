3a92e57059b1a48fa640dee9bb2218ca12a2465c
Sidstumple <cydstumpel@msn.com>
2018-03-07 16:42:25 +0100

    add structure to footer

---
+++ src/components/footer.vue
@@ -0,0 +1,47 @@
+<template>
+	<footer class="footer">
+		<section class="row footer__title">
+			<h3>Not your average Digital Agency</h3>
+		</section>
+
+		<section class="row footer-nav">
+			<ul class="row list--row footer-nav__list">
+				<li class="mobile-hide footer-nav__list-item"><a href="/culture">Culture</a></li>
+				<li class="mobile-hide footer-nav__list-item"><a href="/work">Work</a></li>
+				<li class="mobile-hide footer-nav__list-item"><a href="/who-we-are">Who we are</a></li>
+				<li class="mobile-hide footer-nav__list-item"><a href="/what-we-do">What we do</a></li>
+				<li class="footer-nav__list-item"><a href="/news">News</a></li>
+				<li class="footer-nav__list-item"><a href="/careers">Careers</a></li>
+			</ul>
+		</section>
+
+		<section class="row contact">
+				<ul class="column list--column contact__list">
+					<li class="contact__list-item">office@matise.nl</li>
+					<li class="contact__list-item">+31 20 845 37 99</li>
+					<li class="contact__list-item">Entrepotdok 78, Amsterdam</li>
+					<li class="contact__list-item">Open till 6pm today</li>
+				</ul>
+
+				<ul class="column list--column">
+					<li><a href="https://www.instagram.com/matiseamsterdam/">Instagram</a></li>
+					<li><a href="https://www.facebook.com/MatiseAmsterdam/">Facebook</a></li>
+					<li><a href="https://www.linkedin.com/company/matise/">LinkedIn</a></li>
+					<li><a href="https://github.com/MatiseAms">Github</a></li>
+				</ul>
+		</section>
+
+		<section class="row practical">
+			<ul class="list--row practical__list">
+				<li class="practical__list-item">© Matise 2018 — All Rights Reserved</li>
+				<li class="practical__list-item"><a href="#">Terms & conditions</a></li>
+				<li class="practical__list-item"><a href="#">Privacy Policy</a></li>
+			</ul>
+		</section>
+	</footer>
+</template>
+
+<script>
+export default {
+}
+</script>
