d5e9995aca27bb36f769bed60aa7b0de587f9500
Sidstumple <cydstumpel@msn.com>
2018-03-07 18:14:41 +0100

    add mobile and tablet styles

--- src/layouts/FooterElement.vue
+++ src/layouts/FooterElement.vue
@@ -5,7 +5,7 @@
 		</section>
 
 			<ul class="row list--row footer__nav">
-				<li class="mobile-hide footer__nav__list-item"><a href="/culture">Culture</a></li>
+				<li class="mobile-hide footer__nav__list-item footer__nav__list-item--active"><a href="/culture">Culture</a></li>
 				<li class="mobile-hide footer__nav__list-item"><a href="/work">Work</a></li>
 				<li class="mobile-hide footer__nav__list-item"><a href="/who-we-are">Who we are</a></li>
 				<li class="mobile-hide footer__nav__list-item"><a href="/what-we-do">What we do</a></li>
@@ -22,15 +22,15 @@
 				</ul>
 
 				<ul class="column list--column footer__social">
-					<li class="footer__social__item link--external"><a href="https://www.instagram.com/matiseamsterdam/">Instagram</a></li>
-					<li class="footer__social__item link--external"><a href="https://www.facebook.com/MatiseAmsterdam/">Facebook</a></li>
-					<li class="footer__social__item link--external"><a href="https://www.linkedin.com/company/matise/">LinkedIn</a></li>
-					<li class="footer__social__item link--external"><a href="https://github.com/MatiseAms">Github</a></li>
+					<li class="footer__social__item link--external"><a href="https://www.instagram.com/matiseamsterdam/" target="_blank">Instagram</a></li>
+					<li class="footer__social__item link--external"><a href="https://www.facebook.com/MatiseAmsterdam/" target="_blank">Facebook</a></li>
+					<li class="footer__social__item link--external"><a href="https://www.linkedin.com/company/matise/" target="_blank">LinkedIn</a></li>
+					<li class="footer__social__item link--external"><a href="https://github.com/MatiseAms" target="_blank">Github</a></li>
 				</ul>
 		</section>
 
 		<ul class="row list--row footer__practical">
-			<li class="footer__practical__item">© Matise 2018 — All Rights Reserved</li>
+			<li class="footer__practical__item copyright">© Matise 2018 — All Rights Reserved</li>
 			<li class="footer__practical__item"><a href="#">Terms & conditions</a></li>
 			<li class="footer__practical__item"><a href="#">Privacy Policy</a></li>
 		</ul>
