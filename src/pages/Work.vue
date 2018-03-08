5753c10cd97043532059eea15a88a433ae2a1a42
Sidstumple <cydstumpel@msn.com>
2018-03-08 13:30:35 +0100

    add html structure and class dispenser for cards to work page

--- src/pages/Work.vue
+++ src/pages/Work.vue
@@ -1,6 +1,41 @@
 <template>
 	<main-layout>
-		<h3>Hoi dit is work</h3>
+		<article class="work__card">
+			<div class="work__card__image" style="background: url('https://coresites-cdn.factorymedia.com/mpora_new/wp-content/uploads/2016/06/Windsurfing-Board-Gear-Beginner-Holidays.jpg') no-repeat; background-size: cover;"></div>
+			<p class="work__card__description"><span class="work__card__description--bold">Aangenaam Bergen op Zoom</span> Original stories from a ‘gezellige’ community.</p>
+		</article>
+		<article class="work__card">
+			<div class="work__card__image" style="background: url('https://coresites-cdn.factorymedia.com/mpora_new/wp-content/uploads/2016/06/Windsurfing-Board-Gear-Beginner-Holidays.jpg') no-repeat; background-size: cover;"></div>
+			<p class="work__card__description"><span class="work__card__description--bold">Aangenaam Bergen op Zoom</span> Original stories from a ‘gezellige’ community.</p>
+		</article>
+		<article class="work__card">
+			<div class="work__card__image" style="background: url('https://coresites-cdn.factorymedia.com/mpora_new/wp-content/uploads/2016/06/Windsurfing-Board-Gear-Beginner-Holidays.jpg') no-repeat; background-size: cover;"></div>
+			<p class="work__card__description"><span class="work__card__description--bold">Aangenaam Bergen op Zoom</span> Original stories from a ‘gezellige’ community.</p>
+		</article>
+		<article class="work__card">
+			<div class="work__card__image" style="background: url('https://coresites-cdn.factorymedia.com/mpora_new/wp-content/uploads/2016/06/Windsurfing-Board-Gear-Beginner-Holidays.jpg') no-repeat; background-size: cover;"></div>
+			<p class="work__card__description"><span class="work__card__description--bold">Aangenaam Bergen op Zoom</span> Original stories from a ‘gezellige’ community.</p>
+		</article>
+		<article class="work__card">
+			<div class="work__card__image" style="background: url('https://coresites-cdn.factorymedia.com/mpora_new/wp-content/uploads/2016/06/Windsurfing-Board-Gear-Beginner-Holidays.jpg') no-repeat; background-size: cover;"></div>
+			<p class="work__card__description"><span class="work__card__description--bold">Aangenaam Bergen op Zoom</span> Original stories from a ‘gezellige’ community.</p>
+		</article>
+		<article class="work__card">
+			<div class="work__card__image" style="background: url('https://coresites-cdn.factorymedia.com/mpora_new/wp-content/uploads/2016/06/Windsurfing-Board-Gear-Beginner-Holidays.jpg') no-repeat; background-size: cover;"></div>
+			<p class="work__card__description"><span class="work__card__description--bold">Aangenaam Bergen op Zoom</span> Original stories from a ‘gezellige’ community.</p>
+		</article>
+		<article class="work__card">
+			<div class="work__card__image" style="background: url('https://coresites-cdn.factorymedia.com/mpora_new/wp-content/uploads/2016/06/Windsurfing-Board-Gear-Beginner-Holidays.jpg') no-repeat; background-size: cover;"></div>
+			<p class="work__card__description"><span class="work__card__description--bold">Aangenaam Bergen op Zoom</span> Original stories from a ‘gezellige’ community.</p>
+		</article>
+		<article class="work__card">
+			<div class="work__card__image" style="background: url('https://coresites-cdn.factorymedia.com/mpora_new/wp-content/uploads/2016/06/Windsurfing-Board-Gear-Beginner-Holidays.jpg') no-repeat; background-size: cover;"></div>
+			<p class="work__card__description"><span class="work__card__description--bold">Aangenaam Bergen op Zoom</span> Original stories from a ‘gezellige’ community.</p>
+		</article>
+		<article class="work__card">
+			<div class="work__card__image" style="background: url('https://coresites-cdn.factorymedia.com/mpora_new/wp-content/uploads/2016/06/Windsurfing-Board-Gear-Beginner-Holidays.jpg') no-repeat; background-size: cover;"></div>
+			<p class="work__card__description"><span class="work__card__description--bold">Aangenaam Bergen op Zoom</span> Original stories from a ‘gezellige’ community.</p>
+		</article>
 	</main-layout>
 </template>
 
@@ -11,4 +46,23 @@ export default {
 		MainLayout
 	}
 }
+
+setTimeout(function(){
+	let allCards = document.querySelectorAll('.work__card');
+	const customClass = ['medium', 'small', 'large', 'small', 'large', 'medium'];
+	let counter = 0;
+
+	allCards.forEach(function(card){
+		card.classList.add('work__card--' + customClass[counter]);
+		if(counter !== customClass.length -1){
+			counter++;
+		} else{
+			counter = 0;
+		}
+	});
+	console.log(allCards);
+},400)
+
+
+
 </script>
