f0336d331902c9e1a8ea6369495e98478d5697bf
Sonia <sonia@matise.nl>
2018-03-08 11:37:20 +0100

    progress

--- src/layouts/TopNav.vue
+++ src/layouts/TopNav.vue
@@ -8,12 +8,18 @@
         </div>
         <nav id="nav" class="nav">
           <ul>
-            <li class="menu-link"><v-link href="/culture">Culture</v-link></li>
-            <li class="menu-link"><v-link href="/work">Work</v-link></li>
-            <li class="menu-link"><v-link href="/what-we-do">What we do</v-link></li>
-            <li class="menu-link"><v-link href="/who-we-are">Who we are</v-link></li>
+            <li class="menu-link"><v-link href="/culture"><p>Culture</p></v-link></li>
+            <li class="menu-link"><v-link href="/work"><p>Work</p></v-link></li>
+            <li class="menu-link"><v-link href="/what-we-do"><p>What we do</p></v-link></li>
+            <li class="menu-link"><v-link href="/who-we-are"><p>Who we are</p></v-link></li>
             <li id="logo" class="menu-link logo"><v-link href="/">Matise.</v-link></li>
           </ul>
+          <div class="contact-info">
+            <p>Contact</p>
+            <p><a href="mailto:office@matise.nl">office@matise.nl</a></p>
+            <p>+31 20 845 37 99</p>
+            <p>Entrepotdok 78, Amsterdam</p>
+          </div>
         </nav>
       </div>
     </div>
