80b6cdfbd3e3ac880519369f329a2310533ffa7c
Roxanne <roxanne@matise.nl>
2018-03-09 12:17:29 +0100

    static 1 col responsive

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,15 @@
 <template>
 <main-layout>
 	<div class="row center">
-			<div class="column large-18">
-				<div class="holder big shading">
+			<div class="column large-18 medium-full small-full">
+				<div class="holder full">
 					<!-- img paceholder for principle anime-->
-					<img src="../../static/img/placeholder.png" alt="placeholder">
+					<div class="contentHolder">
+						<img src="../../static/img/placeholder.png" alt="placeholder">
+					</div>
 				</div>
 			</div>
-			<div class="column large-12">
+			<div class="column large-12 medium-16 small-16">
 				<h3>Part of you ride</h3>
 				<p>Matise was asked to develop an online concept that would translate BBB’s vision. Simultaneously preserving the brand’s rich heritage and community, whilst also repositioning BBB’s modern vision on cycling. We developed a concept that allows BBB to present its products, without losing touch of the what a sunset ride in late spring feels like. In this way visitors may inform themselves about the specs on a helmet, while feeling the thrill of crossing down a hill.</p>
 			</div>
