50073ecdfad1602abe9a46a5b7f371357ea34814
Sonia <sonia@matise.nl>
2018-03-12 17:15:09 +0100

    adding logo to mobile too

--- src/layouts/TopNav.vue
+++ src/layouts/TopNav.vue
@@ -3,19 +3,19 @@
     <div class="row center">
       <div class="column column-20">
         <input id="menu-trigger" type="checkbox"/>
-        <div class="sidekick"><a class="logo"></a>
+        <div class="sidekick"><a class="logo"><img src="../assets/images/logo.svg"></a>
           <label class="trigger" for="menu-trigger"><span></span></label>
         </div>
         <nav id="nav" class="nav">
           <ul>
-            <li class="menu-link"><v-link href="/culture"><p>Culture</p></v-link></li>
-            <li class="menu-link"><v-link href="/work"><p>Work</p></v-link></li>
-            <li class="menu-link"><v-link href="/what-we-do"><p>What we do</p></v-link></li>
-            <li class="menu-link"><v-link href="/who-we-are"><p>Who we are</p></v-link></li>
+            <li class="menu-link main-link"><v-link href="/culture"><p>Culture</p></v-link></li>
+            <li class="menu-link main-link"><v-link href="/work"><p>Work</p></v-link></li>
+            <li class="menu-link main-link"><v-link href="/what-we-do"><p>What we do</p></v-link></li>
+            <li class="menu-link main-link"><v-link href="/who-we-are"><p>Who we are</p></v-link></li>
             <li id="logo" class="menu-link logo"><v-link href="/"><img src="../assets/images/logo.svg"></v-link></li>
           </ul>
           <div class="contact-info">
-            <p>Contact</p>
+            <p><b>Contact</b></p>
             <ul>
               <li class="menu-link"><p><a href="mailto:office@matise.nl">office@matise.nl</a></p></li>
               <li class="menu-link"><p>+31 20 845 37 99</p></li>
