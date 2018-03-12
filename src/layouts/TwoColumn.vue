89799728cf256164f3dfdac1a9e4048ab1444505
Roxanne <roxanne@matise.nl>
2018-03-12 16:49:10 +0100

    clean up html

---
+++ src/layouts/TwoColumn.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="row center">
+	<div class="column large-14 medium-20 small-full">
+		<carousel :mouseDrag="false">
+			<slide>
+				<img src="../../static/img/placeholderMob.png" alt="placeholder">
+			</slide>
+			<slide>
+				<img src="../../static/img/placeholderMob.png" alt="placeholder">
+			</slide>
+		</carousel>
+	</div>
+	<div class="column large-12 medium-16 small-16">
+		<h4><span>Responsive?</span>In the design process we made the choice to kill responsive. A website should be fluid and adapt to all screen sizes from our perspective.</h4>
+	</div>
+</div>
+</template>
+
+<style lang="scss">
+	@import '../assets/scss/app.scss';
+	// .VueCarousel-inner{
+	// 	.VueCarousel-slide{ border: solid 1px red; width: grid(6); height: 100%;
+	// 		img{ width: grid(6); height: 100%; }
+	// 	}
+	// }
+</style>
+
+<script>
+import VLink from '../components/VLink.vue'
+import MainLayout from '../layouts/MainLayout.vue'
+
+import {
+	Carousel,
+	Slide
+} from 'vue-carousel';
+
+export default {
+	components: {
+		VLink,
+		MainLayout,
+		Carousel,
+		Slide
+	},
+}
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,32 +1,7 @@
 <template>
-	<main-layout>
-		<div class="row center">
-			<div class="column large-18 medium-full small-full">
-				<div class="holder full">
-					<!-- img paceholder for principle anime-->
-					<div class="contentHolder">
-						<img src="../../static/img/placeholder.png" alt="placeholder">
-					</div>
-				</div>
-			</div>
-			<div class="column large-12 medium-16 small-16">
-				<h3>Part of you ride</h3>
-				<p>Matise was asked to develop an online concept that would translate BBB’s vision. Simultaneously preserving the brand’s rich heritage and community, whilst also repositioning BBB’s modern vision on cycling. We developed a concept that allows BBB to
-					present its products, without losing touch of the what a sunset ride in late spring feels like. In this way visitors may inform themselves about the specs on a helmet, while feeling the thrill of crossing down a hill.</p>
-			</div>
-		</div>
-		<div class="row center">
-			<div class="column large-14 medium-20 small-full">
-				<slick ref="slick" :options="slickOptions">
-					<img src="../../static/img/placeholderMob.png" alt="placeholder">
-					<img src="../../static/img/placeholderMob.png" alt="placeholder">
-				</slick>
-			</div>
-			<div class="column large-12 medium-16 small-16">
-				<h4><span>Responsive?</span>In the design process we made the choice to kill responsive. A website should be fluid and adapt to all screen sizes from our perspective.</h4>
-			</div>
-		</div>
-	</main-layout>
+<main-layout>
+
+</main-layout>
 </template>
 
 <script>
@@ -35,7 +10,6 @@ import MainLayout from '../layouts/MainLayout.vue'
 export default {
 	components: {
 		MainLayout,
-	}
+	},
 }
-
 </script>
