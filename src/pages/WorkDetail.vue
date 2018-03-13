858b3bd6819c08f1e3e99824077708e24bfc4653
Sonia <sonia@matise.nl>
2018-03-13 15:15:24 +0100

    progress hover

--- src/pages/WorkDetail.vue
+++ src/pages/WorkDetail.vue
@@ -6,9 +6,11 @@
 			</div>
 		</div>
 		<section class="next-case" style="background-color: #524d40;">
-			<div class="row center">
+			<div class="row center textCenter">
 				<div class="column column-24">
-					<p>Next case - BBB Cycling</p>
+					<div id="case-text" class="case-text">
+						<p><span id="next-case-text">Next case</span> BBB Cycling</p>
+					</div>
 				</div>
 			</div>
 		</section>
@@ -21,4 +23,19 @@ export default {
 		MainLayout
 	}
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+	var caseText = document.getElementById('case-text');
+	var nextCaseText = document.getElementById('next-case-text');
+	nextCaseText.style.transition = 'opacity 300ms';
+
+	caseText.addEventListener('mouseover', function(){
+		fade();
+	});
+
+	function fade(){
+		nextCaseText.style.opacity = 0; 
+		setTimeout(function(){ nextCaseText.classList.add('hidden'); }, 500);
+	}
+});
 </script>
