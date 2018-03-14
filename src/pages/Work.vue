53d4f68328a248da101b8213b93aad7155d2a563
Sidstumple <cydstumpel@msn.com>
2018-03-14 16:09:55 +0100

    MW-19 #comment Add responsive styles

--- src/pages/Work.vue
+++ src/pages/Work.vue
@@ -13,21 +13,22 @@
 			</article>
 			<article class="work__card">
 				<div class="work__card__image-container">
-					<img class="work__card__image" src="/static/img/nielson.png">
-				</div>
-				<p class="work__card__description"><span class="work__card__description--bold">Sony Music Netherlands</span> A new digital home for Nielson, an artist that reinvented himself.</p>
-			</article>
-			<article class="work__card">
-				<div class="work__card__image-container">
-					<img class="work__card__image" src="/static/img/bbb.png">
-				</div>
-				<p class="work__card__description"><span class="work__card__description--bold">BBB Cycling</span> This is what happens when commerce and emotion collide and come together.</p>
-			</article>
-			<article class="work__card">
-				<div class="work__card__image-container">
 					<img class="work__card__image" src="/static/img/boz.png">
 				</div>
 				<p class="work__card__description"><span class="work__card__description--bold">Aangenaam Bergen op Zoom</span> Original stories from a ‘gezellige’ community.</p>
+			</article>
+			<article class="work__card">
+				<div class="work__card__image-container">
+					<img class="work__card__image" src="/static/img/wadden.png">
+				</div>
+				<p class="work__card__description"><span class="work__card__description--bold">Expeditie Wadden</span> A journey through time and space.</p>
+			</article>
+
+			<article class="work__card">
+				<div class="work__card__image-container">
+					<img class="work__card__image" src="/static/img/nielson.png">
+				</div>
+				<p class="work__card__description"><span class="work__card__description--bold">Sony Music Netherlands</span> A new digital home for Nielson, an artist that reinvented himself.</p>
 			</article>
 			<article class="work__card">
 				<div class="work__card__image-container">
@@ -37,21 +38,22 @@
 			</article>
 			<article class="work__card">
 				<div class="work__card__image-container">
-					<img class="work__card__image" src="/static/img/mtv.png">
-				</div>
-				<p class="work__card__description"><span class="work__card__description--bold">Viacom Out of Office</span> Getting your out of office as fun as actually being out of office.</p>
-			</article>
-			<article class="work__card">
-				<div class="work__card__image-container">
-					<img class="work__card__image" src="/static/img/wadden.png">
-				</div>
-				<p class="work__card__description"><span class="work__card__description--bold">Expeditie Wadden</span> A journey through time and space.</p>
-			</article>
-			<article class="work__card">
-				<div class="work__card__image-container">
 					<img class="work__card__image" src="/static/img/boz.png">
 				</div>
 				<p class="work__card__description"><span class="work__card__description--bold">Stadsmakelaar</span>Finding the place of your dreams in Bergen op Zoom.</p>
+			</article>
+
+			<article class="work__card">
+				<div class="work__card__image-container">
+					<img class="work__card__image" src="/static/img/bbb.png">
+				</div>
+				<p class="work__card__description"><span class="work__card__description--bold">BBB Cycling</span> This is what happens when commerce and emotion collide and come together.</p>
+			</article>
+			<article class="work__card">
+				<div class="work__card__image-container">
+					<img class="work__card__image" src="/static/img/mtv.png">
+				</div>
+				<p class="work__card__description"><span class="work__card__description--bold">Viacom Out of Office</span> Getting your out of office as fun as actually being out of office.</p>
 			</article>
 			<article class="work__card">
 				<div class="work__card__image-container">
@@ -74,73 +76,94 @@ export default {
 setTimeout(function(){
 	let allCards = document.querySelectorAll('.work__card__image-container');
 	let images = document.querySelectorAll('.work__card__image');
-	// const customClass = ['medium', 'small', 'large', 'small', 'large', 'medium'];
-
 
 	const column1 = ['medium', 'small'];
 	const column2 = ['small', 'large'];
 	const column3 = ['large', 'medium'];
 
 	let counter = 0;
+	let columns;
 	let column;
 	let activeColumn;
 	let columnClass;
+	let savedWidth = window.innerWidth;
 
 	const background = document.querySelector('.background-canvas');
+	window.addEventListener('resize', function(){
+		checkColumns();
+	})
 
-	const columns = images.length / 3;
-
-	for (let i = 0; i < allCards.length; i++){
-		if (i <= (columns - 1)) {
-			activeColumn = column1;
-			columnClass = 'column1';
-		} else if (i <= (columns * 2) - 1){
-			activeColumn = column2;
-			columnClass = 'column2';
+	let checkColumns = function(){
+		if (window.innerWidth < 375 && savedWidth != 375){
+			columns = images.length;
+			savedWidth = 375;
+			assignColumns(columns)
+		} else if ((window.innerWidth < 960 && window.innerWidth > 375) && savedWidth != 960){
+			columns = images.length / 2;
+			savedWidth = 960;
+			assignColumns(columns)
 		} else {
-			activeColumn = column3;
-			columnClass = 'column3';
-		}
-		if(column !== activeColumn){
-			column = activeColumn;
-			counter = 0;
-		}
-		allCards[i].classList.add('work__card__image--' + activeColumn[counter]);
-		allCards[i].parentElement.classList.add(columnClass);
-
-		if(counter !== column.length -1){
-			counter++;
-		} else{
-			counter = 0;
+			columns = images.length / 3;
+			savedWidth = 1440;
+			assignColumns(columns)
 		}
 	}
-	images.forEach(function(image){
-		let color = getAverageRGB(image);
-		image.addEventListener('mouseover', function () {
-			background.style.background = 'rgb(' + color.r + ',' + color.g + ',' + color.b +')';
-			background.classList.add('background--forward');
-			image.parentElement.parentElement.classList.add('card--hover');
-			checkMouse(image);
-		});
-		let checkMouse = function(image){
-			image.addEventListener('mouseout', function (){
-				background.style.background = 'none';
-				background.classList.remove('background--forward');
-				image.parentElement.parentElement.classList.remove('card--hover');
-			});
-		}
 
-	});
+	let assignColumns = function(columns) {
+		console.log('init', columns);
+		for (let i = 0; i < allCards.length; i++){
+			if (i <= (columns - 1)) {
+				activeColumn = column1;
+				columnClass = 'column1';
+			} else if (i <= Math.round((columns * 2) - 1)){
+				activeColumn = column2;
+				columnClass = 'column2';
+			} else {
+				activeColumn = column3;
+				columnClass = 'column3';
+			}
+			if(column !== activeColumn){
+				column = activeColumn;
+				counter = 0;
+			}
+			allCards[i].classList.add('work__card__image--' + activeColumn[counter]);
+			allCards[i].parentElement.classList = ('work__card ' + columnClass);
+
+			if(counter !== column.length -1){
+				counter++;
+			} else{
+				counter = 0;
+			}
+		}
+	}
+	let onHover = function(){
+		images.forEach(function(image){
+			let color = getAverageRGB(image);
+			image.addEventListener('mouseover', function () {
+				background.style.background = 'rgb(' + color.r + ',' + color.g + ',' + color.b +')';
+				background.classList.add('background--forward');
+				image.parentElement.parentElement.classList.add('card--hover');
+				checkMouse(image);
+			});
+		});
+	}
+	let checkMouse = function(image){
+		image.addEventListener('mouseout', function (){
+			background.style.background = 'none';
+			background.classList.remove('background--forward');
+			image.parentElement.parentElement.classList.remove('card--hover');
+		});
+	}
 	function getAverageRGB(imgEl) {
 		var blockSize = 5, // only visit every 5 pixels
-		defaultRGB = {r:0,g:0,b:0}, // for non-supporting envs
-		canvas = document.createElement('canvas'),
-		context = canvas.getContext && canvas.getContext('2d'),
-		data, width, height,
-		i = -4,
-		length,
-		rgb = {r:0,g:0,b:0},
-		count = 0;
+			defaultRGB = {r:0,g:0,b:0}, // for non-supporting envs
+			canvas = document.createElement('canvas'),
+			context = canvas.getContext && canvas.getContext('2d'),
+			data, width, height,
+			i = -4,
+			length,
+			rgb = {r:0,g:0,b:0},
+			count = 0;
 
 		if (!context) {
 			return defaultRGB;
@@ -174,6 +197,8 @@ setTimeout(function(){
 
 		return rgb;
 	}
+	onHover();
+	checkColumns();
 },400)
 
 </script>
