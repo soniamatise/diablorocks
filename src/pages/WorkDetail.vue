8e29a76e380a3ede4e49aec7be57604ff87491fe
Sonia <sonia@matise.nl>
2018-03-15 13:39:56 +0100

    progress animation

--- src/pages/WorkDetail.vue
+++ src/pages/WorkDetail.vue
@@ -5,12 +5,16 @@
 				<p>BBB Cycling — This is what happens when commerce and emotion collide and come together.</p>
 			</div>
 		</div>
-		<section class="next-case" style="background-color: #524d40;">
+		<section id="next-case" class="next-case">
 			<div class="row center textCenter">
 				<div class="column column-24">
-					<div id="case-text" class="case-text">
-						<div class="next-text"><p>Next case —</p></div>
-						<div class="case-text"><p>BBB Cycling</p></div>
+					<div id="case-text-inner" class="case-text-inner">
+						<div class="next-text">
+							<div id="next-text">
+								<p> &nbsp;—&nbsp;Next case</p>
+							</div>
+						</div>
+						<div id="case-text" class="case-text"><p>BBB Cycling</p></div>
 					</div>
 					<div class="case-image"></div>
 				</div>
@@ -25,4 +29,36 @@ export default {
 		MainLayout
 	}
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+	var caseTextInner = document.getElementById('case-text-inner');
+	var nextText = document.getElementById('next-text');
+	var caseText = document.getElementById('case-text');
+	var bgDiv = document.getElementById('next-case');
+
+	function toggleWidth() {
+		var currentWidthInner = caseTextInner.offsetWidth;
+		var currentWidthNext = nextText.offsetWidth;
+		var currentWidthCase = caseText.offsetWidth;
+		var currentWidthNextParent = nextText.parentElement;
+
+		currentWidthNextParent.style.width = currentWidthNext + 'px';
+		nextText.style.width = currentWidthNext + 'px';
+		caseText.style.width = 'calc(1px + ' + currentWidthCase + 'px)';
+		caseTextInner.style.width = 'calc(1px + ' + currentWidthInner + 'px)';
+
+	}
+	toggleWidth();
+	function bgColorOfCase() {
+		bgDiv.style.backgroundColor = '#524d40';
+	}
+	function bgColorOfNormal() {
+		bgDiv.style.backgroundColor = 'black';
+	}
+
+
+	window.addEventListener('resize', toggleWidth);
+	bgDiv.addEventListener('mouseover', bgColorOfCase);
+	bgDiv.addEventListener('mouseout', bgColorOfNormal);
+});
 </script>
