f23f0d1b85f6302e561111997ce22bee4b2af2b7
Sonia <sonia@matise.nl>
2018-03-19 16:49:41 +0100

    progress video play

--- src/pages/Work.vue
+++ src/pages/Work.vue
@@ -1,6 +1,24 @@
 <template>
-	<main-layout>
+	<main-layout class="work">
 		<h3>Hoi dit is work</h3>
+		<section class="video-container">
+			<div class="row center">
+				<div class="column column-24">
+					<video class="video" width="100%" height="100%" controls>
+						<source src="../assets/videos/demo.mp4" type="video/mp4">
+					</video>
+					<div class="controls">
+						<div class="pause" id="pause"></div>
+						<div class="timeline"></div>
+						<div class="fullsize"></div>
+						<div class="volume"></div>
+					</div>
+					<div class="textCenter play" id="play">
+						<h2><b>Play</b></h2>
+					</div>
+				</div>
+			</div>
+		</section>
 	</main-layout>
 </template>
 
@@ -11,4 +29,17 @@ export default {
 		MainLayout
 	}
 }
+
+// let video = document.querySelectorAll('.video');
+
+document.addEventListener('DOMContentLoaded',  () => {
+	const videos = document.getElementsByClassName('video');
+	for(let i = 0; i < videos.length; i++)
+	{
+		const play = document.getElementById('play');
+		play.addEventListener('click', () => {
+			videos[i].play();
+		})
+	}
+});
 </script>
