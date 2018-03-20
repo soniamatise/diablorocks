19f3fccf713d2332be84ff32791fb033bd1bbeea
Maurits Meester <maurits@matise.nl>
2018-03-20 20:13:19 +0100

    typewriter component

---
+++ components/TypeWriter.vue
@@ -0,0 +1,46 @@
+<template>
+	<h1 id="typeWriterTitle"><slot></slot></h1>
+</template>
+
+<script>
+export default {
+	props: ['delay'],
+	// mounted is sort of like domready
+	mounted() {
+		let self = this;
+		// get data from slot
+		const dataText = self.$slots.default[0].text;
+		console.log(self.$el.innerHTML);
+		// console.log(this.$slots.default[0].innerHTML)
+		if(!self.delay){
+			self.typeWriter(dataText, 0);
+		}else{
+			setTimeout(function() {
+				self.typeWriter(dataText, 0);
+			}, self.delay);
+		}
+
+	},
+	methods: {
+		typeWriter: function(text,i, fnCallback){
+			let self = this;
+
+			if (i < (text.length)) {
+				self.$el.innerHTML = text.substring(0, i+1) +'<span aria-hidden="true" id="typestroke"></span>';
+
+				setTimeout(function() {
+					self.typeWriter(text, i + 1)
+				}, 250);
+			}else{
+				var typestroke = document.getElementById('typestroke');
+				typestroke.style.border = 'white';
+				// content moet dus ook white worden?
+				// var index = 0, length = content.length;
+				// for ( ; index < length; index++) {
+				// 	content[index].style.opacity = '1';
+				// }
+			}
+		}
+	}
+}
+</script>
