b1ca3d329a1bd1a3802d2d78dd8862444db414cb
Sidstumple <cydstumpel@msn.com>
2018-03-20 17:24:25 +0100

    Work on click and mouseover functions of component

--- components/WorkCard.vue
+++ components/WorkCard.vue
@@ -1,8 +1,10 @@
 <template>
-	<article class="work__card " :case="caseName">
+
+
+	<article :class="'work__card column-' + columnNr + ' ' + expand" :case="caseName" :style="custom">
 		<!-- <nuxt-link :to="'/work/' + slug "> -->
 			<div :class="'work__card__image-container work__card__image-container--'+ size ">
-				<img v-on:click="clickAnim()" v-on:hover="onHover()" class="work__card__image" :src="image">
+				<img v-on:click="clickAnim()" v-on:mouseout="onHover()" class="work__card__image" :src="image">
 			</div>
 		<!-- </nuxt-link> -->
 		<p class="work__card__description"><span class="work__card__description--bold">{{ client }}</span> {{ description }}</p>
@@ -11,16 +13,40 @@
 
 <script>
 export default {
-	props: ['caseName', 'size', 'image', 'client', 'description', 'slug'],
+	props: ['columnNr', 'caseName', 'size', 'image', 'client', 'description', 'slug', 'custom', 'expand'],
 	methods: {
-    clickAnim: function (event) {
-      // `this` inside methods points to the Vue instance
-      alert('Hello ' + this + '!')
-      // `event` is the native DOM event
-      if (event) {
-        alert(event.target.tagName)
-      }
-    }
+    clickAnim: function () {
+    	console.log(this._self);
+			// if (deviceSize === 'large'){
+			// 	if (e.target.parentElement.classList.contains('work__card__image-container--small')) {
+			// 		e.target.parentElement.parentElement.style = `--height: ${getGrid(7)}vw; --width: ${getGrid(6)}vw; --margin: ${getGrid(9)}vw; --top: ${getGrid(2)}vw;`
+			// 	} else if (e.target.parentElement.classList.contains('work__card__image-container--medium')) {
+			// 		e.target.parentElement.parentElement.style = `--height: ${getGrid(8)}vw; --width: ${getGrid(6)}vw; --margin: ${getGrid(9)}vw; --top: ${getGrid(2)}vw;`
+			// 	} else {
+			// 		e.target.parentElement.parentElement.style = `--height: ${getGrid(9)}vw; --width: ${getGrid(6)}vw; --margin: ${getGrid(9)}vw; --top: ${getGrid(2)}vw;`
+			// 	}
+			// } else if (deviceSize === 'medium'){
+			// 	if (e.target.parentElement.classList.contains('work__card__image-container--small')) {
+			// 		e.target.parentElement.parentElement.style = `--height: ${getGrid(10)}vw; --width: ${getGrid(9.25)}vw; --margin: ${getGrid(7.375)}vw; --top: ${getGrid(4)}vw;`;
+			// 	} else if (e.target.parentElement.classList.contains('work__card__image-container--medium')) {
+			// 		e.target.parentElement.parentElement.style = `--height: ${getGrid(12)}vw; --width: ${getGrid(9.25)}vw; --margin: ${getGrid(7.375)}vw; --top: ${getGrid(4)}vw;`;
+			// 	} else {
+					// e.target.parentElement.parentElement.style = `--height: ${getGrid(14)}vw; --width: ${getGrid(9.25)}vw; --margin: ${getGrid(7.375)}vw; --top: ${getGrid(4)}vw;`;
+				// }
+			// } else {
+			// document.querySelectorAll('article[case=' + this.caseName +']') = `--height: ${getGrid(20)}vw; --width: ${getGrid(20)}vw; --margin: ${getGrid(2)}vw; --top: ${getGrid(3)}vw;`;
+			// }
+			// document.querySelectorAll('article[case=' + this.caseName +']').classList.add('expandCard');
+			// e.target.parentElement.classList.add('expandImage');
+			// document.querySelector('background').classList.add('expandBackground');
+			// image.parentElement.parentElement.classList.remove('card--hover');
+			function getGrid(value){
+				return (100 / 24) * value;
+			}
+    },
+		onHover: function (){
+			console.log(this);
+		},
   }
 }
 
--- pages/work.vue
+++ pages/work.vue
@@ -2,62 +2,79 @@
 <template>
 	<main-layout>
 
-		<div class="background-canvas"></div>
-
+<div class="background-canvas"></div>
 		<section class="work__grid">
 
-			<WorkCard caseName="kalkhoff"
-			size="small"
+			<WorkCard
+			columnNr="1"
+			caseName="kalkhoff"
+			size="medium"
 			image="http://media-channel.nationalgeographic.com/media/uploads/photos/content/video/2017/04/03/912762947881_912762947881.jpg"
 			client="Kalkhoff Bikes"
 			description="When your bikes are eye-catchers, your website should be too."
 			slug="kalkhoff-bikes"/>
-			<WorkCard caseName="aangenaam"
+			<WorkCard
+			columnNr="1"
+			caseName="aangenaam"
 			size="small"
 			image="http://www.wdkx.com/wdkxwp/wp/wp-content/uploads/2017/09/animals_hero_giraffe_1_0.jpg"
 			client="Aangenaam Bergen op Zoom"
 			description="Original stories from a ‘gezellige’ community."
 			slug="aangenaam-bergen-op-zoom"/>
-			<WorkCard caseName="expeditiewadden"
-			size="small"
+			<WorkCard
+			columnNr="1"
+			caseName="expeditiewadden"
+			size="medium"
 			image="http://www.pbs.org/wnet/nature/files/2016/10/giraffe_episode_thumb.jpg"
 			client="Expeditie Wadden"
 			description="A journey through time and space."
 			slug="expeditie-wadden"/>
 
-			<WorkCard caseName="nielson"
+			<WorkCard
+			columnNr="2"
+			caseName="nielson"
 			size="small"
 			image="https://lechantelteam.files.wordpress.com/2013/10/giraffe15342.jpg"
 			client="Sony Music Netherlands"
 			description="A new digital home for Nielson, an artist that reinvented himself."
 			slug="nielson-air"/>
-			<WorkCard caseName="ticketchaser"
-			size="small"
+			<WorkCard
+			columnNr="2"
+			caseName="ticketchaser"
+			size="large"
 			image="https://cdn.suwalls.com/wallpapers/funny/giraffe-on-a-tree-trunk-42410-1920x1080.jpg"
 			client="Sony Music Netherlands & DVLM"
 			description="Scavenger hunts are the future of giveaway campaigns."
 			slug="ticketchaser"/>
-			<WorkCard caseName="stadsmakelaar"
+			<WorkCard
+			columnNr="2"
+			caseName="stadsmakelaar"
 			size="small"
 			image="http://www.4usky.com/data/out/38/164324255-giraffe-wallpapers.jpg"
 			client="Stadsmakelaar"
 			description="Finding the place of your dreams in Bergen op Zoom."
 			slug="stadsmakelaar"/>
 
-			<WorkCard caseName="bbbcycling"
-			size="small"
+			<WorkCard
+			columnNr="3"
+			caseName="bbbcycling"
+			size="large"
 			image="http://www.wallpapers13.com/wp-content/uploads/2016/05/Giraffe-often-feeds-on-pasture-high-shrublands-trees-with-green-leaves-Desktop-Wallpaper-Backgrounds-HD-3840x2400-1920x1080.jpg"
 			client="BBB Cycling"
 			description="This is what happens when commerce and emotion collide and come together."
 			slug="bbb-cycling"/>
-			<WorkCard caseName="viacom"
-			size="small"
+			<WorkCard
+			columnNr="3"
+			caseName="viacom"
+			size="medium"
 			image="http://www.wallpapers4u.org/wp-content/uploads/giraffe_sky_muzzle_profile_59848_1920x1080.jpg"
 			client="Viacom Out of Office"
 			description="Getting your out of office as fun as actually being out of office."
 			slug="viacom-out-of-office"/>
-			<WorkCard caseName="natwerk"
-			size="small"
+			<WorkCard
+			columnNr="3"
+			caseName="natwerk"
+			size="large"
 			image="http://deskbg.com/i/c/1920x1080/wpp/21/21588/giraffe-running-desktop-background.jpg"
 			client="Natwerk"
 			description="I GIF this website a 10/10."
@@ -77,6 +94,8 @@ export default {
 		MainLayout,
 		WorkCard
 	},
+	methods: {
+	}
 }
 
 
@@ -102,118 +121,116 @@ export default {
 // 	// let images = document.querySelectorAll('.work__card__image');
 //
 // 	// const background = document.querySelector('.background-canvas');
-// 	const column1 = ['medium', 'small'];
-// 	const column2 = ['small', 'large'];
-// 	const column3 = ['large', 'medium'];
+
 //
-// 	let counter = 0;
-// 	let columns;
-// 	let column;
-// 	let activeColumn;
-// 	let columnClass;
-// 	let deviceSize;
-// 	// let savedWidth = window.innerWidth;
-//
-// 	// window.addEventListener('resize', function(){
-// 	// 	checkColumns();
-// 	// });
-// 	// let checkColumns = function(){
-// 	// 	if (window.innerWidth < 750 && savedWidth != 750){
-// 	// 		columns = images.length;
-// 	// 		savedWidth = 750;
-// 	// 		assignColumns(columns);
-// 	// 		deviceSize = 'small';
-// 	// 	} else if ((window.innerWidth < 960 && window.innerWidth > 750) && savedWidth != 960){
-// 	// 		columns = Math.ceil(images.length / 2);
-// 	// 		savedWidth = 960;
-// 	// 		assignColumns(columns);
-// 	// 		deviceSize = 'medium';
-// 	// 	} else {
-// 	// 		columns = images.length / 3;
-// 	// 		savedWidth = 1440;
-// 	// 		assignColumns(columns);
-// 	// 		deviceSize = 'large';
-// 	// 	}
-// 	// }
-// 	// let assignColumns = function(columns) {
-// 	// 	for (let i = 0; i < allCards.length; i++){
-// 	// 		if (i <= (columns - 1)) {
-// 	// 			activeColumn = column1;
-// 	// 			columnClass = 'column1';
-// 	// 		} else if (i <= (columns * 2) - 1){
-// 	// 			activeColumn = column2;
-// 	// 			columnClass = 'column2';
-// 	// 		} else {
-// 	// 			activeColumn = column3;
-// 	// 			columnClass = 'column3';
-// 	// 		}
-// 	// 		if(column !== activeColumn){
-// 	// 			column = activeColumn;
-// 	// 			counter = 0;
-// 	// 		}
-// 	// 		allCards[i].classList.add('work__card__image-container--' + activeColumn[counter]);
-// 	// 		allCards[i].parentElement.classList = ('work__card ' + columnClass);
-// 	//
-// 	// 		if(counter !== column.length -1){
-// 	// 			counter++;
-// 	// 		} else{
-// 	// 			counter = 0;
-// 	// 		}
-// 	// 	}
-// 	// }
-// 	let onHover = function(){
-// 		images.forEach(function(image){
-// 			clickAnim(image);
-// 			image.addEventListener('mouseover', function () {
-// 				background.classList.add('bg-' + image.parentElement.parentElement.getAttribute('case'));
-// 				if(!background.classList.contains('expandBackground')){
-// 					image.parentElement.parentElement.classList.add('card--hover');
-// 					background.classList.add('background--forward');
-// 				}
-// 				checkMouse(image);
-// 			});
-// 		});
-// 	}
-// 	let checkMouse = function(image){
-// 		image.addEventListener('mouseout', function (){
-// 			if(!background.classList.contains('expandBackground')){
-// 				background.classList.remove('bg-' + image.parentElement.parentElement.getAttribute('case'));
-// 			}
-// 			background.classList.remove('background--forward');
-// 			image.parentElement.parentElement.classList.remove('card--hover');
-// 		});
-// 	}
-// 	let clickAnim = function(image){
-// 		image.addEventListener('click', function(e) {
-// 			if (deviceSize === 'large'){
-// 				if (e.target.parentElement.classList.contains('work__card__image-container--small')) {
-// 					e.target.parentElement.parentElement.style = `--height: ${getGrid(7)}vw; --width: ${getGrid(6)}vw; --margin: ${getGrid(9)}vw; --top: ${getGrid(2)}vw;`
-// 				} else if (e.target.parentElement.classList.contains('work__card__image-container--medium')) {
-// 					e.target.parentElement.parentElement.style = `--height: ${getGrid(8)}vw; --width: ${getGrid(6)}vw; --margin: ${getGrid(9)}vw; --top: ${getGrid(2)}vw;`
-// 				} else {
-// 					e.target.parentElement.parentElement.style = `--height: ${getGrid(9)}vw; --width: ${getGrid(6)}vw; --margin: ${getGrid(9)}vw; --top: ${getGrid(2)}vw;`
-// 				}
-// 			} else if (deviceSize === 'medium'){
-// 				if (e.target.parentElement.classList.contains('work__card__image-container--small')) {
-// 					e.target.parentElement.parentElement.style = `--height: ${getGrid(10)}vw; --width: ${getGrid(9.25)}vw; --margin: ${getGrid(7.375)}vw; --top: ${getGrid(4)}vw;`;
-// 				} else if (e.target.parentElement.classList.contains('work__card__image-container--medium')) {
-// 					e.target.parentElement.parentElement.style = `--height: ${getGrid(12)}vw; --width: ${getGrid(9.25)}vw; --margin: ${getGrid(7.375)}vw; --top: ${getGrid(4)}vw;`;
-// 				} else {
-// 					e.target.parentElement.parentElement.style = `--height: ${getGrid(14)}vw; --width: ${getGrid(9.25)}vw; --margin: ${getGrid(7.375)}vw; --top: ${getGrid(4)}vw;`;
-// 				}
-// 			} else {
-// 				e.target.parentElement.parentElement.style = `--height: ${getGrid(20)}vw; --width: ${getGrid(20)}vw; --margin: ${getGrid(2)}vw; --top: ${getGrid(3)}vw;`;
-// 			}
-// 			e.target.parentElement.parentElement.classList.add('expandCard');
-// 			e.target.parentElement.classList.add('expandImage');
-// 			background.classList.add('expandBackground');
-// 			image.parentElement.parentElement.classList.remove('card--hover');
-// 		});
-// 	}
-// 	function getGrid(value){
-// 		return (100 / 24) * value;
-// 	}
-// 	onHover();
-// 	checkColumns();
+	// let counter = 0;
+	// let columns;
+	// let column;
+	// let activeColumn;
+	// let columnClass;
+	// let deviceSize;
+	// let savedWidth = window.innerWidth;
+	//
+	// window.addEventListener('resize', function(){
+	// 	checkColumns();
+	// });
+	// let checkColumns = function(){
+	// 	if (window.innerWidth < 750 && savedWidth != 750){
+	// 		columns = images.length;
+	// 		savedWidth = 750;
+	// 		assignColumns(columns);
+	// 		deviceSize = 'small';
+	// 	} else if ((window.innerWidth < 960 && window.innerWidth > 750) && savedWidth != 960){
+	// 		columns = Math.ceil(images.length / 2);
+	// 		savedWidth = 960;
+	// 		assignColumns(columns);
+	// 		deviceSize = 'medium';
+	// 	} else {
+	// 		columns = images.length / 3;
+	// 		savedWidth = 1440;
+	// 		assignColumns(columns);
+	// 		deviceSize = 'large';
+	// 	}
+	// }
+	// let assignColumns = function(columns) {
+	// 	for (let i = 0; i < allCards.length; i++){
+	// 		if (i <= (columns - 1)) {
+	// 			activeColumn = column1;
+	// 			columnClass = 'column1';
+	// 		} else if (i <= (columns * 2) - 1){
+	// 			activeColumn = column2;
+	// 			columnClass = 'column2';
+	// 		} else {
+	// 			activeColumn = column3;
+	// 			columnClass = 'column3';
+	// 		}
+	// 		if(column !== activeColumn){
+	// 			column = activeColumn;
+	// 			counter = 0;
+	// 		}
+	// 		allCards[i].classList.add('work__card__image-container--' + activeColumn[counter]);
+	// 		allCards[i].parentElement.classList = ('work__card ' + columnClass);
+	//
+	// 		if(counter !== column.length -1){
+	// 			counter++;
+	// 		} else{
+	// 			counter = 0;
+	// 		}
+	// 	}
+	// }
+	// let onHover = function(){
+	// 	images.forEach(function(image){
+	// 		clickAnim(image);
+	// 		image.addEventListener('mouseover', function () {
+	// 			background.classList.add('bg-' + image.parentElement.parentElement.getAttribute('case'));
+	// 			if(!background.classList.contains('expandBackground')){
+	// 				image.parentElement.parentElement.classList.add('card--hover');
+	// 				background.classList.add('background--forward');
+	// 			}
+	// 			checkMouse(image);
+	// 		});
+	// 	});
+	// }
+	// let checkMouse = function(image){
+	// 	image.addEventListener('mouseout', function (){
+	// 		if(!background.classList.contains('expandBackground')){
+	// 			background.classList.remove('bg-' + image.parentElement.parentElement.getAttribute('case'));
+	// 		}
+	// 		background.classList.remove('background--forward');
+	// 		image.parentElement.parentElement.classList.remove('card--hover');
+	// 	});
+	// }
+	// let clickAnim = function(image){
+	// 	image.addEventListener('click', function(e) {
+	// 		if (deviceSize === 'large'){
+	// 			if (e.target.parentElement.classList.contains('work__card__image-container--small')) {
+	// 				e.target.parentElement.parentElement.style = `--height: ${getGrid(7)}vw; --width: ${getGrid(6)}vw; --margin: ${getGrid(9)}vw; --top: ${getGrid(2)}vw;`
+	// 			} else if (e.target.parentElement.classList.contains('work__card__image-container--medium')) {
+	// 				e.target.parentElement.parentElement.style = `--height: ${getGrid(8)}vw; --width: ${getGrid(6)}vw; --margin: ${getGrid(9)}vw; --top: ${getGrid(2)}vw;`
+	// 			} else {
+	// 				e.target.parentElement.parentElement.style = `--height: ${getGrid(9)}vw; --width: ${getGrid(6)}vw; --margin: ${getGrid(9)}vw; --top: ${getGrid(2)}vw;`
+	// 			}
+	// 		} else if (deviceSize === 'medium'){
+	// 			if (e.target.parentElement.classList.contains('work__card__image-container--small')) {
+	// 				e.target.parentElement.parentElement.style = `--height: ${getGrid(10)}vw; --width: ${getGrid(9.25)}vw; --margin: ${getGrid(7.375)}vw; --top: ${getGrid(4)}vw;`;
+	// 			} else if (e.target.parentElement.classList.contains('work__card__image-container--medium')) {
+	// 				e.target.parentElement.parentElement.style = `--height: ${getGrid(12)}vw; --width: ${getGrid(9.25)}vw; --margin: ${getGrid(7.375)}vw; --top: ${getGrid(4)}vw;`;
+	// 			} else {
+	// 				e.target.parentElement.parentElement.style = `--height: ${getGrid(14)}vw; --width: ${getGrid(9.25)}vw; --margin: ${getGrid(7.375)}vw; --top: ${getGrid(4)}vw;`;
+	// 			}
+	// 		} else {
+	// 			e.target.parentElement.parentElement.style = `--height: ${getGrid(20)}vw; --width: ${getGrid(20)}vw; --margin: ${getGrid(2)}vw; --top: ${getGrid(3)}vw;`;
+	// 		}
+	// 		e.target.parentElement.parentElement.classList.add('expandCard');
+	// 		e.target.parentElement.classList.add('expandImage');
+	// 		background.classList.add('expandBackground');
+	// 		image.parentElement.parentElement.classList.remove('card--hover');
+	// 	});
+	// }
+	// function getGrid(value){
+	// 	return (100 / 24) * value;
+	// }
+	// onHover();
+	// checkColumns();
 
 </script>
