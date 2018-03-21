3b1d9fa3010b47d0a6c9b121c8724832ac2e68aa
Sonia <sonia@matise.nl>
2018-03-21 17:14:16 +0100

    become a component

---
+++ components/CaseHeading.vue
@@ -0,0 +1,27 @@
+<template>
+	<section class="intro">
+		<div class="intro-text">
+			<div class="row center">
+				<div class="column small-full medium-16 large-12">
+					<p><span>{{ caseName }}</span> {{ caseQuote }}</p>
+				</div>
+			</div>
+		</div>
+		<div class="intro-image" :style="{ 'background-image': 'url(' + image + ')' }">
+			<div class="row center">
+				<div class="column column-24">
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script>
+export default {
+	props: ['caseName', 'caseQuote', 'image'],
+	mounted() {
+	},
+	methods: {
+	}
+}
+</script>
--- pages/work/_detail.vue
+++ pages/work/_detail.vue
@@ -1,9 +1,10 @@
 <template>
-	<main-layout>
-		<h1>Hoi</h1>
-		<payoff-credits></payoff-credits>
-		<one-column></one-column>
-		<two-column></two-column>
+	<main-layout class="work-detail">
+		<case-heading 
+			caseName="BBB Cycling"
+			caseQuote="This is what happens when commerce and emotion collide and come together."
+			image="/_nuxt/img/bbb.22d3225.png"
+		/>
 	</main-layout>
 </template>
 
@@ -12,12 +13,14 @@ import MainLayout from '~/layouts/MainLayout.vue'
 import OneColumn from '~/layouts/OneColumn.vue'
 import TwoColumn from '~/layouts/TwoColumn.vue'
 import PayoffCredits from '~/layouts/PayoffCredits.vue'
+import CaseHeading from '@/components/CaseHeading.vue'
 export default {
 	components: {
 		OneColumn,
 		TwoColumn,
 		PayoffCredits,
-		MainLayout
+		MainLayout,
+		CaseHeading
 	}
 }
 </script>
