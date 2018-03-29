3b594bf1a5403f61e90c522062d015b9fc8482b7
Maurits Meester <maurits@matise.nl>
2018-03-29 16:59:24 +0200

    Maurits’ magie voor Tim 🤡

--- components/NextCase.vue
+++ components/NextCase.vue
@@ -1,6 +1,6 @@
 <template>
 <section id="next-case" class="nextCase">
-	<div class="hoverHolder" id="mouseOver" @mouseover="bgOfCase()" @mouseleave="bgToNormal()" :case="caseName">
+	<div class="hoverHolder" id="mouseOver" @mouseenter="bgOfCase()" @mouseleave="bgToNormal()" :case="caseName">
 		<div class="contentHolder">
 			<div class="textHolder textHolderLeft">
 				<div id="textholder-left" class="mask">
@@ -19,7 +19,7 @@
 			</div>
 		</div>
 	</div>
-	<div id="case-image" class="caseImage">
+	<div id="caseImage" class="caseImage">
 		<img :src="image">
 	</div>
 </section>
@@ -28,13 +28,19 @@
 <script>
 export default {
 	props: ['caseName', 'image', 'caseColor'],
+	data() {
+		return {
+			Velocity: this.$velocity
+		}
+	},
 	methods: {
 		bgOfCase: function() {
+
+			console.log('hoi of case');
 
 			//case color on mouseover
 			const bgDiv = document.getElementById('next-case');
 			bgDiv.style.backgroundColor = this.caseColor;
-
 
 			//text animation on mouseover
 			const leftText = document.getElementById('textholder-left');
@@ -44,16 +50,17 @@ export default {
 			rightText.style.transform = 'translateX(' + -currentWidthInner + 'px)';
 
 			//case image animation on mouseover
-			const caseImage = document.getElementById('case-image');
+			const caseImage = document.getElementById('caseImage');
 
 			//get grid size for image height on mouseover
 			const getGrid = window.innerWidth / 12;
 
 			//case image animation on mouseover
-			caseImage.style.transform = 'translate(-50%,' + -getGrid + 'px)';
+			this.Velocity(caseImage, { transform: 'translate(-50%, ' + -getGrid + 'px)' }, 600, [180, 16]);
+			// caseImage.style.transform = 'translate(-50%,' + -getGrid + 'px)';
 		},
-		bgToNormal: () => {
-
+		bgToNormal: function() {
+			console.log('hoi to normal');
 			//black color on mouseleave
 			const bgDiv = document.getElementById('next-case');
 			bgDiv.style.backgroundColor = 'black';
@@ -63,8 +70,9 @@ export default {
 			rightText.style.transform = 'translateX(0px)';
 
 			//case image animation on mouseleave
-			const caseImage = document.getElementById('case-image');
-			caseImage.style.transform = 'translate(-50%, 0)';
+			const caseImage = document.getElementById('caseImage');
+			this.Velocity(caseImage, { transform: 'translate(-50%, 0)' }, 600, [180, 16]);
+			// caseImage.style.transform = 'translate(-50%, 0)';
 		},
 	},
 	mounted: () => {
