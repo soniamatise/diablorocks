55b35e1673a9794ed80a349b0cecccdd3f64c0f9
Sidstumple <cydstumpel@msn.com>
2018-03-30 11:37:01 +0200

    wip

---
+++ components/HomeIntro.vue
@@ -0,0 +1,18 @@
+<template>
+	<section class="home-intro">
+
+	</section>
+</template>
+
+<script>
+export default {
+	data: {
+		return {
+				text: ["Empathic Branding", 'Creative Strategy', 'Innovative Digital', 'Aspiring rental box mogul']
+		}
+	}
+}
+</script>
+
+<style lang="css">
+</style>
