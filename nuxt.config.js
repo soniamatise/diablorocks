ba27d2a6892807124f9173274bdd1b910fb1802f
Sidstumple <cydstumpel@msn.com>
2018-03-30 11:55:37 +0200

    Fix error on css variables

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,50 +1,76 @@
 module.exports = {
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: 'Matise | Creative Digital Agency',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Matise 2018 in Nuxt' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#000000' },
-	plugins: [
-		{ src: '~plugins/swiper.js', ssr: false },
-    { src: '~plugins/velocity.js', ssr: false },
-	],
-  /*
-  ** Build configuration
-  */
-  build: {
-    /*
-    ** Run ESLint on save
-    */
-    extend (config, { isDev, isClient }) {
-      if (isDev && isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    },
-    vendor: ['velocity-animate']
-  },
-	css: [{
-		src: '~assets/scss/app.scss',
-		lang: 'scss'
+	/*
+	 ** Headers of the page
+	 */
+	head: {
+		title: 'Matise | Creative Digital Agency',
+		meta: [{
+				charset: 'utf-8'
+			},
+			{
+				name: 'viewport',
+				content: 'width=device-width, initial-scale=1'
+			},
+			{
+				hid: 'description',
+				name: 'description',
+				content: 'Matise 2018 in Nuxt'
+			}
+		],
+		link: [{
+			rel: 'icon',
+			type: 'image/x-icon',
+			href: '/favicon.ico'
+		}]
 	},
-	{
-		src: 'swiper/dist/css/swiper.css'
-	}]
+	/*
+	 ** Customize the progress bar color
+	 */
+	loading: {
+		color: '#000000'
+	},
+	plugins: [{
+			src: '~plugins/swiper.js',
+			ssr: false
+		},
+		{
+			src: '~plugins/velocity.js',
+			ssr: false
+		},
+	],
+	/*
+	 ** Build configuration
+	 */
+	build: {
+		postcss: {
+			plugins: {
+				'postcss-custom-properties': false
+			}
+		},
+		/*
+		 ** Run ESLint on save
+		 */
+		extend(config, {
+			isDev,
+			isClient
+		}) {
+			if (isDev && isClient) {
+				config.module.rules.push({
+					enforce: 'pre',
+					test: /\.(js|vue)$/,
+					loader: 'eslint-loader',
+					exclude: /(node_modules)/
+				})
+			}
+		},
+		vendor: ['velocity-animate']
+	},
+	css: [{
+			src: '~assets/scss/app.scss',
+			lang: 'scss'
+		},
+		{
+			src: 'swiper/dist/css/swiper.css'
+		}
+	]
 }
