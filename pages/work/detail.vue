12553df586369275d2072e11eb4043c843a193b0
Roxanne <roxanne@matise.nl>
2018-04-05 13:39:48 +0200

    work in progress

--- pages/work/detail.vue
+++ pages/work/detail.vue
@@ -7,9 +7,10 @@
 			caseColor="#524d40"
 		/>
 		<payoff-credits/>
-		<icons-caption :images="icons.images" timeout="1000" limit="8"
+		<icons-caption
 		caseColor="#81919a"
 		/>
+		<!-- :images="icons.images" timeout="1000" limit="8" -->
 		<one-column/>
 		<two-column/>
 		<video-player
@@ -38,30 +39,30 @@ import VideoPlayer from '~/components/VideoPlayer.vue'
 export default {
 	data() {
 		return{
-			icons: {
-				images: [
-					{ src: 'https://picsum.photos/200/300/?image=0', title: 'test1' },
-					{ src: 'https://picsum.photos/300/300/?image=1', title: 'test2' },
-					{ src: 'https://picsum.photos/330/300/?image=2', title: 'test3' },
-					{ src: 'https://picsum.photos/220/300/?image=3', title: 'test4' },
-					{ src: 'https://picsum.photos/230/300/?image=4', title: 'test5' },
-					{ src: 'https://picsum.photos/250/300/?image=5', title: 'test6' },
-					{ src: 'https://picsum.photos/210/300/?image=6', title: 'test7' },
-					{ src: 'https://picsum.photos/200/310/?image=7', title: 'test8' },
-					{ src: 'https://picsum.photos/200/320/?image=8', title: 'test9' },
-					{ src: 'https://picsum.photos/200/330/?image=9', title: 'test10' },
-					{ src: 'https://picsum.photos/200/340/?image=10', title: 'test11' },
-					{ src: 'https://picsum.photos/200/350/?image=11', title: 'test12' },
-					{ src: 'https://picsum.photos/330/300/?image=12', title: 'test13' },
-					{ src: 'https://picsum.photos/220/300/?image=13', title: 'test14' },
-					{ src: 'https://picsum.photos/230/300/?image=14', title: 'test15' },
-					{ src: 'https://picsum.photos/250/300/?image=15', title: 'test16' },
-					{ src: 'https://picsum.photos/210/300/?image=16', title: 'test17' },
-					{ src: 'https://picsum.photos/200/310/?image=17', title: 'test18' },
-					{ src: 'https://picsum.photos/200/320/?image=18', title: 'test19' },
-					{ src: 'https://picsum.photos/200/330/?image=19', title: 'test20' },
-				]
-			}
+			// icons: {
+			// 	images: [
+			// 		{ src: 'https://picsum.photos/200/300/?image=0', title: 'test1' },
+			// 		{ src: 'https://picsum.photos/300/300/?image=1', title: 'test2' },
+			// 		{ src: 'https://picsum.photos/330/300/?image=2', title: 'test3' },
+			// 		{ src: 'https://picsum.photos/220/300/?image=3', title: 'test4' },
+			// 		{ src: 'https://picsum.photos/230/300/?image=4', title: 'test5' },
+			// 		{ src: 'https://picsum.photos/250/300/?image=5', title: 'test6' },
+			// 		{ src: 'https://picsum.photos/210/300/?image=6', title: 'test7' },
+			// 		{ src: 'https://picsum.photos/200/310/?image=7', title: 'test8' },
+			// 		{ src: 'https://picsum.photos/200/320/?image=8', title: 'test9' },
+			// 		{ src: 'https://picsum.photos/200/330/?image=9', title: 'test10' },
+			// 		{ src: 'https://picsum.photos/200/340/?image=10', title: 'test11' },
+			// 		{ src: 'https://picsum.photos/200/350/?image=11', title: 'test12' },
+			// 		{ src: 'https://picsum.photos/330/300/?image=12', title: 'test13' },
+			// 		{ src: 'https://picsum.photos/220/300/?image=13', title: 'test14' },
+			// 		{ src: 'https://picsum.photos/230/300/?image=14', title: 'test15' },
+			// 		{ src: 'https://picsum.photos/250/300/?image=15', title: 'test16' },
+			// 		{ src: 'https://picsum.photos/210/300/?image=16', title: 'test17' },
+			// 		{ src: 'https://picsum.photos/200/310/?image=17', title: 'test18' },
+			// 		{ src: 'https://picsum.photos/200/320/?image=18', title: 'test19' },
+			// 		{ src: 'https://picsum.photos/200/330/?image=19', title: 'test20' },
+			// 	]
+			// }
 		}
 	},
 		components: {
