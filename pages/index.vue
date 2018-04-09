fb0f02384f8c877e365258125c0c3b8209b744b8
Maurits Meester <maurits@matise.nl>
2018-04-09 21:02:59 +0200

    made home dynamic

--- pages/index.vue
+++ pages/index.vue
@@ -4,72 +4,35 @@
 		<logo/>
 		<top-nav/>
 		<home-slider >
-			<home-slider-item
-				caseName="Kalkhoff Bikes"
-				slug="kalkhoff"
-				caseDescription="When your bikes are eye-catchers, your website should be too."
-				:caseImage="images.kalkhoff"
+
+			<home-slider-item v-for="value in cases" :key="value.post.id"
+				:caseName="value.case_fields.client_name"
+				:slug="`work/${value.post.post_name}`"
+				:caseDescription="value.case_fields.case_description"
+				:caseImage="value.case_fields.case_image"
 				caseImageHeight="full-height"
 			/>
-			<home-slider-item
-				caseName="Sony Music Netherlands"
-				slug="nielson"
-				caseDescription="A new digital home for Nielson, an artist that reinvented himself."
-				:caseImage="images.nielson"
-				caseImageHeight="small-height"
-			/>
-			<home-slider-item
-				caseName="BBB Cycling"
-				slug="bbbcycling"
-				caseDescription="This is what happens when commerce and emotion collide and come together."
-				:caseImage="images.bbb"
-				caseImageHeight="medium-height"
-			/>
-			<home-slider-item
-				caseName="Aangenaam Bergen op Zoom"
-				slug="aangenaam"
-				caseDescription="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa."
-				:caseImage="images.boz"
-				caseImageHeight="medium-height"
-			/>
-			<home-slider-item
-				caseName="Sony Music Netherlands &amp; DVLM"
-				slug="ticketchaser"
-				caseDescription="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa."
-				:caseImage="images.ticketchaser"
-				caseImageHeight="small-height"
-			/>
-			<home-slider-item
-				caseName="Viacom Out of Office"
-				slug="viacom"
-				caseDescription="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa."
-				:caseImage="images.viacom"
-				caseImageHeight="full-height"
-			/>
-			<home-slider-item
-				caseName="Expeditie Wadden"
-				slug="expeditiewadden"
-				caseDescription="Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa."
-				:caseImage="images.expeditiewadden"
-				caseImageHeight="small-height"
-			/>
+
 		</home-slider>
 	</main>
 </template>
 
 <script>
+import axios from 'axios'
+
 import TopNav from '~/layouts/TopNav';
 import Logo from '~/layouts/Logo';
 import HomeSlider from '~/components/HomeSlider';
 import HomeSliderItem from '~/components/HomeSliderItem';
-import axios from 'axios'
-
 
 export default {
 	asyncData ({ params }) {
-    return axios.get(`http://api.matise.nl/wp-json/wp/v2/pages/54`)
+    return axios.get(`${process.env.baseUrl}/page/homepage`)
     .then((res) => {
-			// console.log(res.data);
+			console.log(res.data);
+			return {
+				cases: res.data.cases
+			}
     })
   },
 	components: {
@@ -77,22 +40,6 @@ export default {
 		HomeSliderItem,
 		TopNav,
 		Logo
-	},
-	data(){
-		return {
-			images: {
-				kalkhoff: require('~/assets/img/home/Hero_Kalkhoff.jpg'),
-				nielson: require('~/assets/img/home/Hero_Nielsonair.jpg'),
-				bbb: require('~/assets/img/home/Hero_BBB.jpg'),
-				boz: require('~/assets/img/home/Hero_BoZ.jpg'),
-				ticketchaser: require('~/assets/img/home/Hero_TicketChaser.jpg'),
-				viacom: require('~/assets/img/home/Hero_Viacom.jpg'),
-				expeditiewadden: require('~/assets/img/home/Hero_ExpeditieWadden.jpg')
-			}
-		}
-	},
-	mounted(){
-		// console.log(this);
 	},
 	watch: {
 		slideIn: function(){
