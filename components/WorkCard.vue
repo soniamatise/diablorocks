f87450f134e2b5f04946b440be83ae24fc0837e4
Sidstumple <cydstumpel@msn.com>
2018-04-11 15:22:46 +0200

    Add columns

--- components/WorkCard.vue
+++ components/WorkCard.vue
@@ -1,7 +1,7 @@
 <template>
-	<article :class="['work__card column', 'column'+ columnNr, this._data.cardClass, caseName]" :case="caseName" :style="this._data.customStyle">
+	<article :class="['work__card column' , this._data.cardClass, caseName]" :case="caseName" :style="this._data.customStyle">
 		<!-- <nuxt-link :to="'/work/' + slug "> -->
-		<div :class="'work__card__image-container work__card__image-container--'+ size " :style="'background: url(' + image + ') no-repeat; background-size: 67.5vw; background-position: center;'">
+		<div class="work__card__image-container " :style="'background: url(' + image + ') no-repeat; background-size: 67.5vw; background-position: center;'">
 		</div>
 		<!-- </nuxt-link> -->
 		<p class="work__card__description"><span class="work__card__description--bold">{{ client }}</span> {{ description }}</p>
@@ -12,20 +12,27 @@
 export default {
 	data() {
 		return {
-			cardClass: '',
-			deviceSize: '',
-			custom: '',
-			target: '',
-			targetSize: '',
-			customStyle: '',
-			case: '',
-			scrollPosition: null
+			cardClass: null,
+			deviceSize: null,
+			custom: null,
+			target: null,
+			targetSize: null,
+			customStyle: null,
+			case: null,
+			scrollPosition: null,
+			caseCount: null,
+			columnCount: null,
+			column1: [],
+			column2: [],
+			column3: [],
+			class1: ['medium', 'small'],
+			class2: ['small', 'large'],
+			class3: ['large', 'medium']
 		}
 	},
-	props: ['columnNr', 'caseName', 'size', 'image', 'client', 'description', 'slug', 'color'],
+	props: [ 'caseName', 'image', 'client', 'description', 'slug', 'color'],
 	methods: {
 		expandCard: function(item) {
-			console.log(item);
 			this._data.target = item.size;
 			this._data.case = item.caseName;
 			this._data.cardClass = 'expandCard';
@@ -39,63 +46,113 @@ export default {
 			if (this._data.cardClass !== 'expandCard') {
 				this._data.cardClass = '';
 			}
+		},
+		checkCaseCount: function(caseCount){
+			this._data.caseCount = caseCount;
+		},
+		checkColumns: function(viewWidth, savedWidth){
+			let self = this;
+			let cards = document.querySelectorAll('.work__card');
+			let deviceSize;
+			let targetSize;
+			let cases = cards.length;
+			let activeColumn;
+			let columnClass;
+			let columns;
+
+			let assignColumns = function(columns){
+				for (let i = 0; i < cases; i++){
+					if(i <= (columns - 1) ) {
+						cards[i].classList.add('column1');
+						self._data.column1.push(cards[i]);
+					} else if (i > (columns - 1) && i <= ((columns * 2) - 1)) {
+						cards[i].classList.add('column2');
+						self._data.column2.push(cards[i]);
+					} else{
+						cards[i].classList.add('column3');
+						self._data.column3.push(cards[i]);
+					}
+					if (i == (cases - 1)) {
+						addSizes();
+					}
+				}
+			}
+
+			let addSizes = function(){
+				console.log(self._data.column1);
+				let column1 = self._data.column1;
+				let column2 = self._data.column2;
+				let column3 = self._data.column3;
+
+				if(column3 != ''){
+					column1.forEach(function(card){
+						card.
+					})
+
+				}
+
+
+
+			}
+
+			if (viewWidth < 750 && savedWidth != 750){
+				savedWidth = 750;
+				deviceSize = 'small';
+				targetSize = 'small';
+
+				self._data.deviceSize = deviceSize;
+				self._data.targetSize = targetSize;
+				self._data.columnCount = 1;
+				assignColumns(1);
+			} else if ((window.innerWidth < 960 && window.innerWidth > 750) && savedWidth != 960){
+				// device is medium
+				columns = Math.ceil(cases / 2);
+				savedWidth = 960;
+				deviceSize = 'medium';
+
+				// if (self.$props.size === 'small') {
+				// 	targetSize = 'medium-small';
+				// } else if (self.$props.size === 'medium') {
+				// 	targetSize = 'medium-medium';
+				// } else if (self.$props.size === 'large') {
+				// 	targetSize = 'medium-large';
+				// }
+				self._data.deviceSize = deviceSize;
+				self._data.targetSize = targetSize;
+				self._data.columnCount = columns;
+				assignColumns(columns);
+
+			} else {
+				// device is large
+				columns = Math.ceil(cases / 3);
+				savedWidth = 1440;
+				deviceSize = 'large';
+				// if (self.$props.size === 'small') {
+				// 	targetSize = 'large-small';
+				// } else if (self.$props.size === 'medium') {
+				// 	targetSize = 'large-medium';
+				// } else if (self.$props.size === 'large') {
+				// 	targetSize = 'large-large';
+				// }
+				self._data.deviceSize = deviceSize;
+				self._data.targetSize = targetSize;
+				self._data.columnCount = columns;
+				assignColumns(columns);
+			}
 		}
 	},
 	mounted() {
-		let savedWidth = window.innerWidth;
 		let self = this;
-		let deviceSize;
-		let targetSize;
-		let counter = 0;
-		// TODO: replace images with dynamically loaded items from backend
-		let images = 9;
-		let columns;
-		let activeColumn;
-		let columnClass;
+		let savedWidth = window.innerWidth;
+		let cases = this._data.caseCount;
 		window.addEventListener('resize', function(){
-			checkColumns();
+			this.checkColumns(window.innerWidth, savedWidth);
 		});
 		window.addEventListener('scroll', function(){
 			self._data.scrollPosition = window.pageYOffset;
 		});
-
-		let checkColumns = function(){
-			if (window.innerWidth < 750 && savedWidth != 750){
-				columns = images.length;
-				savedWidth = 750;
-				deviceSize = 'small';
-				targetSize = 'small';
-				self._data.deviceSize = deviceSize;
-				self._data.targetSize = targetSize;
-			} else if ((window.innerWidth < 960 && window.innerWidth > 750) && savedWidth != 960){
-				columns = Math.ceil(images.length / 2);
-				savedWidth = 960;
-				deviceSize = 'medium';
-				if (self.$props.size === 'small') {
-					targetSize = 'medium-small';
-				} else if (self.$props.size === 'medium') {
-					targetSize = 'medium-medium';
-				} else if (self.$props.size === 'large') {
-					targetSize = 'medium-large';
-				}
-				self._data.deviceSize = deviceSize;
-				self._data.targetSize = targetSize;
-			} else {
-				columns = images.length / 3;
-				savedWidth = 1440;
-				deviceSize = 'large';
-				self._data.deviceSize = deviceSize;
-				if (self.$props.size === 'small') {
-					targetSize = 'large-small';
-				} else if (self.$props.size === 'medium') {
-					targetSize = 'large-medium';
-				} else if (self.$props.size === 'large') {
-					targetSize = 'large-large';
-				}
-				self._data.targetSize = targetSize;
-			}
-		}
-		checkColumns();
+		this.checkColumns(window.innerWidth, savedWidth);
+		// work__card__image-container--
 	},
 	watch: {
 		target: function (){
--- pages/work/index.vue
+++ pages/work/index.vue
@@ -3,7 +3,7 @@
 
 		<section class="intro">
 			<type-writer
-			heading="Work Work"
+			heading="Werk werk werk"
 			sub="Our latest and greatest for brands we believe in."
 			v-on:doneTyping="showContent"
 			/>
@@ -12,12 +12,9 @@
 		<div :class="[this._data.background ,'background-canvas']" :style="style"></div>
 
 		<section class="work__grid content" v-bind:class="{displayContent: displayContent}">
-
 			<WorkCard v-for="value in cases" :key="value.id"
 				:ref="value.slug"
-				:columnNr="value.id"
 				:caseName="value.slug"
-				size="medium"
 				:image="value._embedded['wp:featuredmedia'][0].source_url"
 				:client="value.client_name"
 				:description="value.case_description"
@@ -42,12 +39,12 @@
 <script>
 import axios from 'axios'
 
-import MainLayout from '~/layouts/MainLayout.vue'
-import OneColumn from '~/layouts/OneColumn.vue'
-import TwoColumn from '~/layouts/TwoColumn.vue'
-import PayoffCredits from '~/layouts/PayoffCredits.vue'
-import TypeWriter from '~/components/TypeWriter.vue'
-import NextCase from '~/components/NextCase.vue'
+import MainLayout from '~/layouts/MainLayout'
+import OneColumn from '~/layouts/OneColumn'
+import TwoColumn from '~/layouts/TwoColumn'
+import PayoffCredits from '~/layouts/PayoffCredits'
+import TypeWriter from '~/components/TypeWriter'
+import NextCase from '~/components/NextCase'
 import WorkCard from '@/components/WorkCard'
 export default {
 	components: {
@@ -64,29 +61,36 @@ export default {
 			displayContent: false,
 			background: null,
 			click: false,
-			style: { backgroundColor: '#ffffff' }
+			style: { '--background': '#ffffff' },
+			caseCount: null
 		}
 	},
 	methods: {
 		showContent: function(){
 			this.displayContent = true;
+			this.countCases();
+		},
+		countCases: function(){
+			let workCard = this.$refs[this.cases[0].slug];
+			const caseCount = this.cases.length;
+			workCard[0].checkCaseCount(caseCount);
 		},
 		expand: function(item){
 			let workCard = this.$refs[item][0];
 			if (this.displayContent = true){
 				workCard.expandCard(workCard);
-				this._data.background = `bg-${item} expandBackground`;
+				this._data.background = `expandBackground`;
 				this._data.click = true;
 			}
 		},
 		onHover: function(item){
 			if (this.displayContent = true){
 				let workCard = this.$refs[item][0];
-				this.style.backgroundColor = workCard.color;
+				this.style = `--background: ${workCard.color}`;
 				workCard.doHover(item);
 
 				if (this._data.click == false) {
-					this._data.background = `bg-${item} background--forward`;
+					this._data.background = `background--forward`;
 				}
 			}
 		},
@@ -96,7 +100,8 @@ export default {
 			if (this._data.click == false) {
 				this._data.background = '';
 			}
-		}
+		},
+
 	},
 	asyncData ({ params }) {
     return axios.get(`${process.env.baseUrl}/case?_embed`)
