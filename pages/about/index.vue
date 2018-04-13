3f5c404294c2f8051a748adfcdb5ea720ed93855
Maurits Meester <maurits@matise.nl>
2018-04-13 16:38:09 +0200

    voorbeeldje voor seb

---
+++ pages/about/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<main-layout>
+		<h1>{{page.title.rendered}}</h1>
+	</main-layout>
+</template>
+
+<script>
+import axios from 'axios'
+import MainLayout from '~/layouts/MainLayout.vue'
+
+export default {
+	components: {
+		MainLayout
+	},
+	asyncData () {
+    return axios.get(`${process.env.baseUrl}/wp/v2/pages?slug=about&_embed`)
+    .then((res) => {
+      console.log(res.data[0]);
+			return {
+        page: res.data[0]
+      }
+    })
+  }
+}
+</script>
