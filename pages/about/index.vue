8fefe6f5e37b70246cf8cbe0fe030d00aa21681d
seb <seb@matise.nl>
2018-04-17 11:26:28 +0200

    whoops did not commet everything

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,36 +1,39 @@
+
+<!-- very basic data import -->
 <template>
 	<main-layout>
 		<h1>{{page.title.rendered}}</h1>
 
+		<!-- loop trough layouts -->
 		<section v-for="value in layouts" :key="value.id" :class="value.acf_fc_layout">
+			<!-- layout to generate slider -->
 			<div v-if="value.acf_fc_layout==='gallery'" class="gallery">
+				<!-- just data -->
 				<span v-for="image in value.image_holder" :key="image.id" >
 					url: {{ image.url }}
 					title {{image.title}}
 					width: {{image.width }}
 					height: {{image.height}}
-
 				</span>
 			</div>
 
+			<!-- team member bloc, image, name and function -->
 			<div v-if="value.acf_fc_layout==='team_block'" class="team_member">
 				<span v-for="team_member in value.team_member" :key="team_member.id" >
-
+					image:{{ team_member.team_member_photo.url }}
 					name: {{ team_member.team_member_name }}
 					function {{ team_member.team_member_function }}
 
-
 				</span>
 			</div>
 
-			{{value.acf_fc_layout}}
+			<!-- simple text block -->
 			<title-text v-if="value.acf_fc_layout==='simpel_text'" >
 				<h3 v-if="value.block_title" slot="header">{{value.block_title}}</h3>
 				<div class="simple_text_content" v-html="value.block_text"></div>
 			</title-text>
+
 		</section>
-
-
 	</main-layout>
 
 
@@ -48,12 +51,9 @@ export default {
 	asyncData () {
     return axios.get(`${process.env.baseUrl}/wp/v2/pages?slug=about&_embed`)
     .then((res) => {
-      console.log(res.data[0]['layout'][2]);
-			return {
-        page: res.data[0],
-		layouts :res.data[0]['layout'],
-
-
+		return {
+        	page: res.data[0],
+			layouts :res.data[0]['layout'],
       }
     })
   }
