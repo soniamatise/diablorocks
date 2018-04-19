4483c618ed95878f1ecfe9136ab9098dcbdfea04
Roxanne <roxanne@matise.nl>
2018-04-19 14:36:50 +0200

    show content animation

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -10,62 +10,66 @@
 			/>
 		</section>
 
-		<!-- loop trough layouts -->
-		<section v-for="value in layouts" :key="value.id" :class="value.acf_fc_layout">
-			<!-- layout to generate slider -->
-			<div v-if="value.acf_fc_layout==='gallery'" class="gallery">
-				<!-- swiper -->
-				<div class="holder" v-swiper:mySwiper="swiperOption">
-					<!-- swiper wrapper -->
-					<div class="swiper-wrapper">
-						<!-- swiper slide -->
-						<div class="swiper-slide" v-for="image in value.image_holder" :key="image.id" >
-							<div class="content_container">
-								<div class="image_holder">
-									<img :src="image.url" />
-							<!-- url: {{ image.url }} -->
-							<!-- title {{image.title}} -->
-							<!-- width: {{image.width }}
-							height: {{image.height}} -->
-								</div>
+		<section class="about">
+
+			<!-- loop trough layouts -->
+			<section v-for="value in layouts" :key="value.id" :class="value.acf_fc_layout">
+				<!-- layout to generate slider -->
+				<div v-if="value.acf_fc_layout==='gallery'" class="gallery">
+					<!-- swiper -->
+					<div class="holder" v-swiper:mySwiper="swiperOption">
+						<!-- swiper wrapper -->
+						<div class="swiper-wrapper">
+							<!-- swiper slide -->
+							<div class="swiper-slide" v-for="image in value.image_holder" :key="image.id" >
+								<div class="content_container">
+									<div class="image_holder">
+										<img :src="image.url" />
+								<!-- url: {{ image.url }} -->
+								<!-- title {{image.title}} -->
+								<!-- width: {{image.width }}
+								height: {{image.height}} -->
+									</div>
+							</div>
 						</div>
-					</div>
-						<!-- end swiper slide -->
-			    </div>
-					<!-- end swiper wrapper -->
-		  	</div>
-			</div>
-
-
-			<!-- simple text block -->
-			<div class="row center" v-if="value.acf_fc_layout==='simpel_text'" >
-				<div class="large-12 column">
-					<h3 v-if="value.block_title" slot=“header”>{{value.block_title}}</h3>
-					<div class="simple_text_content" v-html="value.block_text"></div>
+							<!-- end swiper slide -->
+				    </div>
+						<!-- end swiper wrapper -->
+			  	</div>
 				</div>
-			</div>
+
+
+				<!-- simple text block -->
+				<div class="row center" v-if="value.acf_fc_layout==='simpel_text'" >
+					<div class="large-12 column">
+						<h3 v-if="value.block_title" slot=“header”>{{value.block_title}}</h3>
+						<div class="simple_text_content" v-html="value.block_text"></div>
+					</div>
+				</div>
 
 
 
-			<!-- team member block, image, name and function -->
-			<section v-if="value.acf_fc_layout==='team_block'" class="team_member">
-				<ul>
-					<li  v-for="team_member in value.team_member" :key="team_member.id" >
-						<div class="picture_frame">
-							<img :src="team_member.team_member_photo.url" />
-						</div>
-						<div class="member_info">
-							<p class="bold"> {{ team_member.team_member_name }} </p>
-							<p class="gray"> {{ team_member.team_member_function }} </p>
-						</div>
-					</li>
-				</ul>
-				<!-- <span v-for="team_member in value.team_member" :key="team_member.id" >
-					image:{{ team_member.team_member_photo.url }}
-					name: {{ team_member.team_member_name }}
-					function {{ team_member.team_member_function }}
-					<img :src="team_member.team_member_photo.sizes.thumbnail" ref="imgActive" />
-				</span> -->
+				<!-- team member block, image, name and function -->
+				<section v-if="value.acf_fc_layout==='team_block'" class="team_member">
+					<ul>
+						<li  v-for="team_member in value.team_member" :key="team_member.id" >
+							<div class="picture_frame">
+								<img :src="team_member.team_member_photo.url" />
+							</div>
+							<div class="member_info">
+								<p class="bold"> {{ team_member.team_member_name }} </p>
+								<p class="gray"> {{ team_member.team_member_function }} </p>
+							</div>
+						</li>
+					</ul>
+					<!-- <span v-for="team_member in value.team_member" :key="team_member.id" >
+						image:{{ team_member.team_member_photo.url }}
+						name: {{ team_member.team_member_name }}
+						function {{ team_member.team_member_function }}
+						<img :src="team_member.team_member_photo.sizes.thumbnail" ref="imgActive" />
+					</span> -->
+				</section>
+
 			</section>
 
 
@@ -107,7 +111,7 @@ export default {
 		showContent: function(){
 			let self = this;
 			self.displayContent = true;
-			let col = document.querySelector('.work__grid');
+			let col = document.querySelector('.about');
 			self.Velocity(col, { transform: 'translateY(0)' }, 600, [180, 16]);
 			setTimeout(function () {
 				col.style = '';
